f230520130106d3f23b7790a3260e627f7642ce2
Gurram2000 <45113957+Gurram2000@users.noreply.github.com>
2021-12-16 08:53:48 +0530

    Add files via upload

---
+++ form.js
@@ -0,0 +1,79 @@
+var selectedRow = null;
+function onFormSubmit(e){
+    event.preventDefault();
+    
+    var formData = readFormData();
+    if(selectedRow === null){
+        alert("HI");
+        
+    }
+    else{
+        alert("HIIIII");
+        updateRecord(formData);
+    }
+    resetForm();
+}
+
+//Retrieve the data
+function readFormData(){
+    var formData = {};
+    formData["redgid"] = document.getElementById("redgid").value;
+    formData["name"] = document.getElementById("name").value;
+    formData["department"] = document.getElementById("department").value;
+    formData["section"] = document.getElementById("section").value;
+    return formData;
+}
+
+//Insert the data
+function insertNewRecord(data){
+    alert(data);
+    var table = document.getElementById('storeList').getElementsByTagName('tbody')[0];
+    alert(table);
+    alert(document.getElementById("storeList"));
+    
+    var newRow = table.insertRow(table.length);
+    
+    var cell1 = newRow.insertCell(0);
+        cell1.innerHTML = data.redgid;
+    var cell2 = newRow.insertCell(1);
+        cell2.innerHTML = data.name;
+    var cell3 = newRow.insertCell(2);
+        cell3.innerHTML =data.department;
+    var cell4 = newRow.insertCell(3);
+        cell4.innerHTML = data.section;
+    var cell5 = newRow.insertCell(4);
+        cell5.innerHTML = `<button onClick='onEdit(this)'>Edit</button> <button onClick='onDelete(this)'>Delete</button>`
+}
+
+//Edit the data
+function onEdit(td){
+    selectedRow = td.parentElement.parentElement;
+    document.getElementById('redgid').value = selectedRow.cells[0].innerHTML;
+    document.getElementById('name').value = selectedRow.cells[1].innerHTML;
+    document.getElementById('department').value = selectedRow.cells[2].innerHTML;
+    document.getElementById('section').value = selectedRow.cells[3].innerHTML;
+}
+
+function updateRecord(formData){
+    selectedRow.cells[0].innerHTML = formData.redgid;
+    selectedRow.cells[1].innerHTML = formData.name;
+    selectedRow.cells[2].innerHTML = formData.department;
+    selectedRow.cells[3].innerHTML = formData.section;
+}
+
+//Delete the data
+function onDelete(td){
+    if(confirm('Do you want to delete this record?')){
+        row = td.parentElement.parentElement;
+        document.getElementById('storeList').deleteRow(row.rowIndex);
+    }
+    resetForm();
+}
+
+//Reset the data
+function resetForm(){
+    document.getElementById('redgid').value = '';
+    document.getElementById('name').value = '';
+    document.getElementById('department').value = '';
+    document.getElementById('section').value = '';
+}
